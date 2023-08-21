3ab7a9b8f00cade8f1b76a5caf9d46ac4d1dcdaf
xiov91 <99517717+xiov91@users.noreply.github.com>
2023-08-21 16:24:22 -0400

    added Lurica and Martin
    the JS logic is fixed and working now, Martin's stats still need the proper picture tho

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-document.getElementById("openModalBtn").addEventListener("click", function() {
-    document.getElementById("profileModal").style.display = "block";
+// Open Buttons
+
+document.getElementById("openMakBtn").addEventListener("click", function () {
+    document.getElementById("makModal").style.display = "block";
 });
 
-document.getElementById("closeModal").addEventListener("click", function() {
-    document.getElementById("profileModal").style.display = "none";
+document.getElementById("openLuriBtn").addEventListener("click", function () {
+    document.getElementById("luriModal").style.display = "block";
 });
 
-window.addEventListener("click", function(event) {
-    if (event.target === document.getElementById("profileModal")) {
-        document.getElementById("profileModal").style.display = "none";
+document.getElementById("openMartinBtn").addEventListener("click", function () {
+    document.getElementById("martinModal").style.display = "block";
+});
+
+// Close Buttons
+
+document.getElementById("closeMakModal").addEventListener("click", function () {
+    document.getElementById("makModal").style.display = "none";
+});
+
+document.getElementById("closeLuriModal").addEventListener("click", function () {
+    document.getElementById("luriModal").style.display = "none";
+});
+
+document.getElementById("closeMartinModal").addEventListener("click", function () {
+    document.getElementById("martinModal").style.display = "none";
+
+});
+
+// Displaying it all
+
+window.addEventListener("click", function (event) {
+    if (event.target === document.getElementById("makModal")) {
+        document.getElementById("makModal").style.display = "none";
+    }
+    else if (event.target === document.getElementById("luriModal")) {
+        document.getElementById("luriModal").style.display = "none";
+    }
+    else if (event.target === document.getElementById("martinModal")) {
+        document.getElementById("martinModal").style.display = "none";
     }
 });
